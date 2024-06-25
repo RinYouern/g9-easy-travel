4db8ccebb1f0eca67956f8eb138e9730e1cbe3ca
LATY.OUN <LATY.OUN@PNC.LAN>
2024-06-25 09:23:10 +0700

    [feat] show list user on admin GE-33

--- frontend vc2 pnc/src/views/Admin/ListUserView.vue
+++ frontend vc2 pnc/src/views/Admin/ListUserView.vue
@@ -1,138 +1,152 @@
 <template>
-  <el-tabs
-    v-model="activeName"
-    type="card"
-    class="demo-tabs"
-    @tab-click="handleClick"
-  >
+  <el-tabs v-model="activeName" type="card" class="demo-tabs" @tab-click="handleClick">
     <el-tab-pane label="Users" name="first">
       <div>
-        <el-table :data="userData" style="width: 100%;">
+        <el-table :data="users" style="width: 100%;">
           <el-table-column label="Picture and Image" align="center">
             <template #default="{ row }">
-              <img :src="row.picture" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
+              <img :src="row.profile" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
             </template>
           </el-table-column>
           <el-table-column prop="name" label="Name" align="center"></el-table-column>
           <el-table-column prop="email" label="Email" align="center"></el-table-column>
           <el-table-column prop="password" label="Password" align="center"></el-table-column>
           <el-table-column label="Action" align="center">
-            <template #default="scope">
-              <el-button type="primary" size="small">Edit</el-button>
-              <el-button type="danger" size="small">Delete</el-button>
+            <template #default="{ row }">
+              <el-button type="primary" size="small" @click="editUser(row)">Edit</el-button>
+              <el-button type="danger" size="small" @click="deleteUser(row)">Delete</el-button>
             </template>
           </el-table-column>
         </el-table>
       </div>
     </el-tab-pane>
-    
+
     <el-tab-pane label="Traveler" name="second">
       <div>
-        <el-table :data="travelerData" style="width: 100%">
+        <el-table :data="travelers" style="width: 100%;">
           <el-table-column label="Picture and Image" align="center">
             <template #default="{ row }">
-              <img :src="row.picture" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
+              <img :src="row.profile" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
             </template>
           </el-table-column>
           <el-table-column prop="name" label="Name" align="center"></el-table-column>
           <el-table-column prop="email" label="Email" align="center"></el-table-column>
           <el-table-column prop="password" label="Password" align="center"></el-table-column>
           <el-table-column label="Action" align="center">
-            <template #default="scope">
-              <el-button type="primary" size="small">Edit</el-button>
-              <el-button type="danger" size="small">Delete</el-button>
+            <template #default="{ row }">
+              <el-button type="primary" size="small" @click="editTraveler(row)">Edit</el-button>
+              <el-button type="danger" size="small" @click="deleteTraveler(row)">Delete</el-button>
             </template>
           </el-table-column>
         </el-table>
       </div>
     </el-tab-pane>
-    
+
     <el-tab-pane label="Companies Car" name="third">
       <div>
-        <el-table :data="companiesCarData" style="width: 100%">
-          <el-table-column label="Picture" align="center">
+        <el-table :data="companiesCar" style="width: 100%;">
+          <el-table-column label="Picture and Image" align="center">
             <template #default="{ row }">
-              <img :src="row.picture" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
+              <img :src="row.profile" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
             </template>
           </el-table-column>
           <el-table-column prop="name" label="Name" align="center"></el-table-column>
           <el-table-column prop="email" label="Email" align="center"></el-table-column>
           <el-table-column prop="password" label="Password" align="center"></el-table-column>
           <el-table-column label="Action" align="center">
-            <template #default="scope">
-              <el-button type="primary" size="small">Edit</el-button>
-              <el-button type="danger" size="small">Delete</el-button>
+            <template #default="{ row }">
+              <el-button type="primary" size="small" @click="editCompanyCar(row)">Edit</el-button>
+              <el-button type="danger" size="small" @click="deleteCompanyCar(row)">Delete</el-button>
             </template>
           </el-table-column>
         </el-table>
       </div>
     </el-tab-pane>
-    
+
     <el-tab-pane label="Hotels" name="fourth">
       <div>
-        <el-table :data="hotelsData" style="width: 100%">
-          <el-table-column label="Picture" align="center">
+        <el-table :data="hotels" style="width: 100%;">
+          <el-table-column label="Picture and Image" align="center">
             <template #default="{ row }">
-              <img :src="row.picture" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
+              <img :src="row.profile" style="max-width: 50px; max-height: 50px; border-radius: 50%;">
             </template>
           </el-table-column>
           <el-table-column prop="name" label="Name" align="center"></el-table-column>
           <el-table-column prop="email" label="Email" align="center"></el-table-column>
           <el-table-column prop="password" label="Password" align="center"></el-table-column>
           <el-table-column label="Action" align="center">
-            <template #default="scope">
-              <el-button type="primary" size="small">Edit</el-button>
-              <el-button type="danger" size="small">Delete</el-button>
+            <template #default="{ row }">
+              <el-button type="primary" size="small" @click="editHotel(row)">Edit</el-button>
+              <el-button type="danger" size="small" @click="deleteHotel(row)">Delete</el-button>
             </template>
           </el-table-column>
         </el-table>
       </div>
     </el-tab-pane>
-    
   </el-tabs>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import { TabsPaneContext } from 'element-plus';
+<script>
+export default {
+  data() {
+    return {
+      activeName: 'first',
+      users: [],
+      travelers: [],
+      companiesCar: [],
+      hotels: []
+    };
+  },
+  methods: {
 
-const activeName = ref('first');
+  },
+  created() {
+  this.fetchUserData();
+  this.fetchTravelerData();
+  this.fetchCompanyCarData();
+  this.fetchHotelData();
+},
 
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event);
-};
+methods: {
+  async fetchUserData() {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/users/all');
+      const data = await response.json();
+      this.users = data.data;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  },
 
-// Sample data for each tab
-const userData = [
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'user1@example.com', password: 'password1', name: 'User 1' },
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'user2@example.com', password: 'password2', name: 'User 2' },
-  // Add more user data as needed
-];
+  async fetchTravelerData() {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/users/role/traveler');
+      const data = await response.json();
+      this.travelers = data.data;
+    } catch (error) {
+      console.error('Error fetching traveler data:', error);
+    }
+  },
 
-const travelerData = [
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'traveler1@example.com', password: 'password1', name: 'Traveler 1' },
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'traveler2@example.com', password: 'password2', name: 'Traveler 2' },
-  // Add more traveler data as needed
-];
+  async fetchCompanyCarData() {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/users/role/carOwner');
+      const data = await response.json();
+      this.companiesCar = data.data;
+    } catch (error) {
+      console.error('Error fetching company car data:', error);
+    }
+  },
 
-const companiesCarData = [
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'company1@example.com', password: 'password1', name: 'Company 1' },
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'company2@example.com', password: 'password2', name: 'Company 2' },
-  // Add more companies car data as needed
-];
-
-const hotelsData = [
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'hotel1@example.com', password: 'password1', name: 'Hotel 1' },
-  { picture: 'https://i.pinimg.com/474x/60/07/0e/60070ed889df308cbe80253e8c36b3a3.jpg', email: 'hotel2@example.com', password: 'password2', name: 'Hotel 2' },
-  // Add more hotels data as needed
-];
-</script>
-
-<style scoped>
-.demo-tabs > .el-tabs__content {
-  padding: 20px;
-  color: #6b778c;
-  font-size: 16px;
+  async fetchHotelData() {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/users/role/hotelOwner');
+      const data = await response.json();
+      this.hotels = data.data;
+    } catch (error) {
+      console.error('Error fetching hotel data:', error);
+    }
+  }
 }
-
-</style>
+};
+</script>
