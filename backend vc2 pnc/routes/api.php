4f6ced684f221645bca3e5f0878054813acddfb4
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-06-25 10:03:12 +0700

    [feat] create api for manage user GE-36

--- backend vc2 pnc/routes/api.php
+++ backend vc2 pnc/routes/api.php
@@ -22,18 +22,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class,'register']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/post/list', [PostController::class, 'index'])->middleware('auth:sanctum');
 Route::delete('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 //user 
 Route::get('/users/role/{role}', [AuthController::class, 'getUsersByRole']);
 Route::get('/users/all', [AuthController::class, 'getAll']);
+Route::put('/users/{id}', [AuthController::class, 'edit']);
+Route::delete('/users/{id}', [AuthController::class, 'delete']);
+Route::get('/user/{id}', [AuthController::class, 'detail']);
 
 // routes/api.php
 Route::post('/addVehicle', [VehicleController::class, 'addVehicle'])->middleware('auth:sanctum');
 
 // booking car
-Route::post('/bookingCar',[BookingController::class, 'bookingCar'])->middleware('auth:sanctum');
+Route::post('/bookingCar', [BookingController::class, 'bookingCar'])->middleware('auth:sanctum');
 Route::get('/recordBooking', [BookingController::class, 'recordOne'])->middleware('auth:sanctum');
 Route::get('/recordAll', [BookingController::class, 'recordAll'])->middleware('auth:sanctum');
