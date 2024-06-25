048887ce5cd8cf36b2d1ecaa65e95ab04d2e7493
LATY.OUN <LATY.OUN@PNC.LAN>
2024-06-25 08:04:44 +0700

    [bug] register and login form

--- frontend vc2 pnc/src/HomePage.vue
+++ frontend vc2 pnc/src/HomePage.vue
@@ -223,7 +223,6 @@
           <a href="#">About</a>
           |
           <a href="#">Contact</a>
-          
         </p>
 
         <p class="footer-company-name">
@@ -249,7 +248,8 @@
       <div class="footer-right">
         <p class="footer-company-about">
           <span>Contact Information</span>
-          <strong>Easy Travel</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, tempora natus fuga eos alias nobis.
+          <strong>Easy Travel</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Iste, tempora natus fuga eos alias nobis.
         </p>
         <div class="footer-icons">
           <a href="#"><i class="fa fa-facebook"></i></a>
@@ -401,151 +401,150 @@ iframe {
 /*footer*/
 
 .footer-distributed {
-    background-color: #060607;
-    box-sizing: border-box;
-    width: 100%;
-    text-align: left;
-    font: bold 16px sans-serif;
-    padding: 50px 50px 60px 50px;
-    margin-top: 50px;
+  background-color: #060607;
+  box-sizing: border-box;
+  width: 100%;
+  text-align: left;
+  font: bold 16px sans-serif;
+  padding: 50px 50px 60px 50px;
+  margin-top: 50px;
 }
 
-.footer-distributed .footer-left, .footer-distributed .footer-center, .footer-distributed .footer-right {
-    display: inline-block;
-    vertical-align: top;
+.footer-distributed .footer-left,
+.footer-distributed .footer-center,
+.footer-distributed .footer-right {
+  display: inline-block;
+  vertical-align: top;
 }
 
 /* Footer left */
 
 .footer-distributed .footer-left {
-    width: 30%;
+  width: 30%;
 }
 
 .footer-distributed h3 {
-    color: #ffffff;
-    font: normal 36px 'Cookie', cursive;
-    margin: 0;
+  color: #ffffff;
+  font:
+    normal 36px 'Cookie',
+    cursive;
+  margin: 0;
 }
 
-
 .footer-distributed h3 span {
-    color: #e0ac1c;
+  color: #e0ac1c;
 }
 
 /* Footer links */
 
 .footer-distributed .footer-links {
-    color: #ffffff;
-    margin: 20px 0 12px;
+  color: #ffffff;
+  margin: 20px 0 12px;
 }
 
 .footer-distributed .footer-links a {
-    display: inline-block;
-    line-height: 1.8;
-    text-decoration: none;
-    color: inherit;
+  display: inline-block;
+  line-height: 1.8;
+  text-decoration: none;
+  color: inherit;
 }
 
 .footer-distributed .footer-company-name {
-    color: #8f9296;
-    font-size: 14px;
-    font-weight: normal;
-    margin: 0;
+  color: #8f9296;
+  font-size: 14px;
+  font-weight: normal;
+  margin: 0;
 }
 
 /* Footer Center */
 
 .footer-distributed .footer-center {
-    width: 35%;
+  width: 35%;
 }
 
 .footer-distributed .footer-center i {
-    background-color: #33383b;
-    color: #ffffff;
-    font-size: 25px;
-    width: 38px;
-    height: 38px;
-    border-radius: 50%;
-    text-align: center;
-    line-height: 42px;
-    margin: 10px 15px;
-    vertical-align: middle;
+  background-color: #33383b;
+  color: #ffffff;
+  font-size: 25px;
+  width: 38px;
+  height: 38px;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 42px;
+  margin: 10px 15px;
+  vertical-align: middle;
 }
 
 .footer-distributed .footer-center i.fa-envelope {
-    font-size: 17px;
-    line-height: 38px;
+  font-size: 17px;
+  line-height: 38px;
 }
 
 .footer-distributed .footer-center p {
-    display: inline-block;
-    color: #ffffff;
-    vertical-align: middle;
-    margin: 0;
+  display: inline-block;
+  color: #ffffff;
+  vertical-align: middle;
+  margin: 0;
 }
 
 .footer-distributed .footer-center p span {
-    display: block;
-    font-weight: normal;
-    font-size: 14px;
-    line-height: 2;
+  display: block;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 2;
 }
 
 .footer-distributed .footer-center p a {
-    color: #e0ac1c;
-    text-decoration: none;
-    ;
+  color: #e0ac1c;
+  text-decoration: none;
 }
 
 /* Footer Right */
 
 .footer-distributed .footer-right {
-    width: 30%;
+  width: 30%;
 }
 
 .footer-distributed .footer-company-about {
-    line-height: 20px;
-    color: #92999f;
-    font-size: 13px;
-    font-weight: normal;
-    margin: 0;
+  line-height: 20px;
+  color: #92999f;
+  font-size: 13px;
+  font-weight: normal;
+  margin: 0;
 }
 
 .footer-distributed .footer-company-about span {
-    display: block;
-    color: #ffffff;
-    font-size: 18px;
-    font-weight: bold;
-    margin-bottom: 20px;
+  display: block;
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 20px;
 }
 
 .footer-distributed .footer-icons {
-    margin-top: 25px;
+  margin-top: 25px;
 }
 
 .footer-distributed .footer-icons a {
-    display: inline-block;
-    width: 35px;
-    height: 35px;
-    cursor: pointer;
-    background-color: #33383b;
-    border-radius: 2px;
-    font-size: 20px;
-    color: #ffffff;
-    text-align: center;
-    line-height: 35px;
-    margin-right: 3px;
-    margin-bottom: 5px;
+  display: inline-block;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  background-color: #33383b;
+  border-radius: 2px;
+  font-size: 20px;
+  color: #ffffff;
+  text-align: center;
+  line-height: 35px;
+  margin-right: 3px;
+  margin-bottom: 5px;
 }
 
 .footer-distributed .footer-icons a:hover {
-    background-color: #3F71EA;
+  background-color: #3f71ea;
 }
 
 .footer-links a:hover {
-    color: #3F71EA;
+  color: #3f71ea;
 }
-
-
-     
 </style>
