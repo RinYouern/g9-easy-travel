5bdfe26258513ad6c2147195511b263782e89054
ryfin65 <ryfin.sok@student.passerellesnumeriques.org>
2024-06-26 10:46:26 +0700

    update form bookingGE-89

--- frontend vc2 pnc/src/views/User/HotelOwner/HotelOwnerView.vue
+++ frontend vc2 pnc/src/views/User/HotelOwner/HotelOwnerView.vue
@@ -107,7 +107,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   *{
     margin: 0;
     padding: 0;
--- frontend vc2 pnc/src/views/User/Traveller/BookingFormView.vue
+++ frontend vc2 pnc/src/views/User/Traveller/BookingFormView.vue
@@ -5,42 +5,69 @@
             <div class="col-md-8">
                 <div class="card d-flex flex-row justify-content-between">
                     <div class="card-body d-flex flex-column justify-content-start">
-                        <h1 class="text-left mt-10px color-white">Welcome {{ carName }}</h1>
+                        <h1 class="text-left mt-10px ">Welcome {{ carName }}</h1>
                     </div>
                     <form @submit.prevent="submitForm" class="card-body d-flex flex-column justify-content-center">
-                        <div class="form-group">
-                            <label for="name">Name:</label>
-                            <input type="text" id="name" v-model="formData.name" class="form-control" required>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="name">Name:</label>
+                                            <input type="text" id="name" v-model="formData.name" class="form-control"
+                                                required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="phoneNumber">Phone Number:</label>
+                                            <input type="tel" id="phoneNumber" v-model="formData.phoneNumber"
+                                                class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="phoneNumber">Phone Number:</label>
-                            <input type="tel" id="phoneNumber" v-model="formData.phoneNumber" class="form-control"
-                                required>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="pickupLocation">Pickup Location:</label>
+                                    <input type="text" id="pickupLocation" v-model="formData.pickupLocation"
+                                        class="form-control" required>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="pickupLocation">Pickup Location:</label>
-                            <input type="text" id="pickupLocation" v-model="formData.pickupLocation"
-                                class="form-control" required>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="pickupDate">Pickup Date:</label>
+                                    <input type="date" id="pickupDate" v-model="formData.pickupDate"
+                                        class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="returnDate">Return Date:</label>
+                                    <input type="date" id="returnDate" v-model="formData.returnDate"
+                                        class="form-control" required>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="pickupDate">Pickup Date:</label>
-                            <input type="date" id="pickupDate" v-model="formData.pickupDate" class="form-control"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <label for="returnDate">Return Date:</label>
-                            <input type="date" id="returnDate" v-model="formData.returnDate" class="form-control"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <label for="numPassengers">Number of Passengers:</label>
-                            <input type="number" id="numPassengers" v-model.number="formData.numPassengers"
-                                class="form-control" min="1" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="totalPrice">Total Price:</label>
-                            <input type="number" id="totalPrice" v-model.number="formData.totalPrice"
-                                class="form-control" required>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="Passengers">Passengers:</label>
+                                    <input type="number" id="Passengers" v-model.number="formData.Passengers"
+                                        class="form-control" min="1" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="totalPrice">Total Price:</label>
+                                    <input type="number" id="totalPrice" v-model.number="formData.totalPrice"
+                                        class="form-control" required>
+                                </div>
+                            </div>
                         </div>
                         <div class="text-center">
                             <button type="submit" class="btn btn-primary">Book Now</button>
@@ -68,7 +95,7 @@ export default {
                 pickupLocation: '',
                 pickupDate: '',
                 returnDate: '',
-                numPassengers: 0,
+                Passengers: 0,
                 totalPrice: 0
             }
         }
@@ -84,8 +111,8 @@ export default {
 
 <style scoped>
 .card {
-    background-image: url(src/assets/image/route.jpg);
-    background-size: cover;
+    /* background-image: url(src/assets/image/route.jpg); */
+    /* background-size: cover; */
     border-radius: 20px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     padding: 20px;
@@ -94,7 +121,7 @@ export default {
 }
 
 .form-group {
-    color: black;
+    /* color: white; */
     margin-bottom: 1.5rem;
 }
 
