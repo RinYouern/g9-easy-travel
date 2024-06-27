24b52b7342241336b6009fb2952224c2d3da2fae
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-06-27 16:59:56 +0700

    [feat] create form add new carGE-39

--- frontend vc2 pnc/src/Components/CarOwner/ListCars.vue
+++ frontend vc2 pnc/src/Components/CarOwner/ListCars.vue
@@ -3,10 +3,15 @@
     <div class="row">
       <sidebar />
       <main class="col-md-9 col-lg-10 main-content p-4">
-        <h2><strong>Vehicles List</strong></h2>
+        <div class="d-flex align-items-center justify-content-between mb-3">
+          <h4 class="text-left text-primary font-weight-bold">
+            <i class="fas fa-car mr-2"></i>Vehicles List
+          </h4>
+          <el-button plain @click="showUserDetails(selectedUser)"> detail </el-button>
+        </div>
         <div class="row">
           <div class="col-md-6 mb-4" v-for="car in cars" :key="car.id">
-            <div class="card mb-3">
+            <div class="card mb-1">
               <div class="row g-0">
                 <div class="col-md-6 d-flex justify-content-center">
                   <img
@@ -32,11 +37,149 @@
       </main>
     </div>
   </div>
+
+  <el-dialog v-model="dialogTableVisible" width="800" style="padding: 5%">
+    <h2 class="text-2xl font-bold mb-6 text-center" style="margin-top: -50px">Add Vehicle</h2>
+    <el-form>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item>
+            <el-input placeholder="Make" v-model="make" size="large" />
+            <i class="bx bxs-car"></i>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item>
+            <el-input
+              placeholder="Traveler Capacity"
+              v-model="traveler_capacity"
+              size="large"
+              type="number"
+            />
+            <i class="bx bxs-user-plus"></i>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item>
+            <el-input placeholder="Year" v-model="year" size="large" type="number" />
+            <i class="bx bxs-calendar"></i>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item>
+            <el-input placeholder="Color" v-model="color" size="large" />
+            <i class="bx bxs-paint"></i>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item>
+            <el-input placeholder="Size" v-model="size" size="large" />
+            <i class="bx bxs-car-garage"></i>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item>
+            <el-input placeholder="Price" v-model="price" size="large" type="number" />
+            <i class="bx bxs-dollar-circle"></i>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row :gutter="20" class="justify-content-center">
+        <el-col :span="20">
+          <el-form-item>
+            <el-input
+              placeholder="Description"
+              v-model="description"
+              size="large"
+              type="textarea"
+            />
+            <i class="bx bxs-edit"></i>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row :gutter="20" class="justify-content-center">
+        <el-col :span="7">
+          <div>
+            <el-button size="large" class="mt-3 w-full" type="warning">Cancel</el-button>
+          </div>
+        </el-col>
+        <el-col :span="7">
+          <div>
+            <el-button
+              size="large"
+              class="mt-3 w-full"
+              :disabled="isSubmitting"
+              type="primary"
+              native-type="submit"
+              >Add Vehicle</el-button
+            >
+          </div>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-dialog>
 </template>
   
-  <script>
+  <script >
 import sidebar from '@/Components/CarOwner/SideBar.vue'
 import { listCarStore } from '@/stores/hevicle-list'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axiosInstance from '@/plugins/axios'
+import { useField, useForm } from 'vee-validate'
+import * as yup from 'yup'
+
+const router = useRouter()
+
+const formSchema = yup.object({
+  make: yup.string().required().label('Make'),
+  traveler_capacity: yup.number().required().label('Traveler Capacity'),
+  year: yup.number().required().label('Year'),
+  color: yup.string().required().label('Color'),
+  size: yup.string().required().label('Size'),
+  price: yup.number().required().label('Price'),
+  description: yup.string().required().label('Description')
+})
+
+const { handleSubmit, isSubmitting } = useForm({
+  initialValues: {
+    make: '',
+    traveler_capacity: '',
+    year: '',
+    color: '',
+    size: '',
+    price: '',
+    description: ''
+  },
+  validationSchema: formSchema
+})
+
+const onSubmit = handleSubmit(async (values) => {
+  try {
+    const { data } = await axiosInstance.post('/addVehicle', values)
+
+    console.log('Vehicle added:', data)
+    dialogTableVisible.value = false
+  } catch (error) {
+    console.error('Error adding vehicle:', error)
+  }
+})
+
+const { value: make, errorMessage: makeError } = useField('make')
+const { value: traveler_capacity, errorMessage: travelerCapacityError } = useField('traveler_capacity')
+const { value: year, errorMessage: yearError } = useField('year')
+const { value: color, errorMessage: colorError } = useField('color')
+const { value: size, errorMessage: sizeError } = useField('size')
+const { value: price, errorMessage: priceError } = useField('price')
+const { value: description, errorMessage: descriptionError } = useField('description')
 
 export default {
   name: 'CarList',
@@ -45,8 +188,9 @@ export default {
   },
   data() {
     return {
-      store: listCarStore()
-    }
+    store: listCarStore(),
+    dialogTableVisible: false, 
+  };
   },
   computed: {
     cars() {
@@ -59,6 +203,9 @@ export default {
   methods: {
     fetchCars() {
       this.store.fetchCars()
+    },
+    showUserDetails() {
+      this.dialogTableVisible = true
     }
   }
 }
@@ -66,5 +213,3 @@ export default {
   
   <style scoped>
 </style>
-  
-  
