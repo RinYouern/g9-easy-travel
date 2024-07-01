09b078a1317db886d9f4952692dc37e45ae850ee
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-07-01 17:51:07 +0700

    [imp] improve style of hotel and booking

--- frontend vc2 pnc/src/Components/CarOwner/ListCars.vue
+++ frontend vc2 pnc/src/Components/CarOwner/ListCars.vue
@@ -1,45 +1,18 @@
-
 <template>
   <div class="container-fluid dashboard">
     <div class="row">
-      <sidebar />
+      <sidebar /> 
       <main class="col-md-9 col-lg-10 main-content p-4">
         <div class="d-flex align-items-center justify-content-between mb-3">
           <h4 class="text-left text-primary font-weight-bold">
             <i class="fas fa-car mr-2"></i>Vehicles List
           </h4>
-          <el-button plain @click="dialogTableVisible = true">Add Vehicle</el-button>
+          <button class="btn btn-primary" lain @click="dialogTableVisible = true">Add Vehicle</button>
         </div>
-        <div class="row">
-          <div class="col-md-6 mb-4" v-for="car in cars" :key="car.id">
-            <div class="card mb-1">
-              <div class="row g-0">
-                <div class="col-md-6 d-flex justify-content-center">
-                  <img
-                    src="https://i.pinimg.com/474x/81/9d/8f/819d8f465bb42ce2f1023e00620d5259.jpg"
-                    alt=""
-                    class="img-fluid rounded-start"
-                  />
-                </div>
-                <div class="col-md-6">
-                  <div class="card-body">
-                    <div class="row text-left">
-                      <p class="card-text">Color: {{ car.color }}</p>
-                      <p class="card-text">Size: {{ car.size }}</p>
-                      <p class="card-text">Price: ${{ car.price }}</p>
-                      <p class="card-text">Traveler Capacity: {{ car.traveler_capacity }}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <vehecles/>
       </main>
-    </div>
-  </div>
-
-  <el-dialog v-model="dialogTableVisible" width="800" style="padding: 5%">
+      
+      <el-dialog v-model="dialogTableVisible" width="800" style="padding: 5%">
     <h2 class="text-2xl font-bold mb-6 text-center" style="margin-top: -50px">Add Vehicle</h2>
     <el-form :model="formData" :rules="formRules" ref="vehicleForm" label-width="120px">
       <el-row :gutter="20">
@@ -96,10 +69,10 @@
         </el-col>
       </el-row>
 
-<el-row :gutter="20" class="justify-content-center">
+      <el-row :gutter="20" class="justify-content-center">
         <el-col :span="7">
           <div>
-            <el-button size="large" class="mt-3 w-full" @click="dialogTableVisible = false">Cancel</el-button>
+            <el-button size="large" class="mt-3 w-full" type="danger" @click="dialogTableVisible = false">Cancel</el-button>
           </div>
         </el-col>
         <el-col :span="7">
@@ -110,13 +83,15 @@
       </el-row>
     </el-form>
   </el-dialog>
+
+    </div>
+  </div>
 </template>
 
 <script>
 import sidebar from '@/Components/CarOwner/SideBar.vue'
-import { listCarStore } from '@/stores/hevicle-list'
+import vehecles from '@/Components/CarOwner/Vehicles.vue'
 import axiosInstance from '@/plugins/axios'
-import { useForm, useField } from 'vee-validate'
 import * as yup from 'yup'
 
 const formSchema = yup.object().shape({
@@ -130,13 +105,13 @@ const formSchema = yup.object().shape({
 })
 
 export default {
-  name: 'CarList',
+  name: 'AddVehicleDialog',
   components: {
-    sidebar
+    sidebar,
+    vehecles
   },
   data() {
     return {
-      store: listCarStore(),
       dialogTableVisible: false,
       formData: {
         make: '',
@@ -146,33 +121,26 @@ export default {
         size: '',
         price: '',
         description: ''
-      }
-    }
-  },
-  computed: {
-    cars() {
-      return this.store.cars
+      },
+      formRules: formSchema,
+      isSubmitting: false
     }
   },
   methods: {
     async onSubmit() {
       try {
+        this.isSubmitting = true
         await this.$refs.vehicleForm.validate()
         const { data } = await axiosInstance.post('/addVehicle', this.formData)
         console.log('Vehicle added:', data)
-        this.dialogTableVisible = false // Close the dialog
-        this.fetchCars() 
+        this.dialogTableVisible = false
+        this.$emit('vehicleAdded')
       } catch (error) {
         console.error('Error adding vehicle:', error)
+      } finally {
+        this.isSubmitting = false
       }
-    },
-    fetchCars() {
-      this.store.fetchCars() 
     }
-  },
-  validationSchema: formSchema
+  }
 }
 </script>
-
-<style scoped>
-</style>
--- frontend vc2 pnc/src/views/User/Traveller/HotelView.vue
+++ frontend vc2 pnc/src/views/User/Traveller/HotelView.vue
@@ -1,71 +1,36 @@
 <template>
-  <div>
-    <navbar></navbar>
-    <div class="bg-light">
-      <div id="content" class="content-wrapper d-flex justify-content-center align-items-center">
-  <div class="p-5 d-flex flex-column align-items-center justify-content-center" id="bg-warning">
-    <div class="text-center mb-4">
-      <h1 class="text-white">Where to Go?</h1>
-      <p class="text-white">Find vehicles available in Cambodia</p>
-    </div>
-    <div class="d-flex justify-content-center">
-      <input
-        type="text"
-        v-model="vehicle"
-        class="form-control"
-        style="width: 300px;"
-        placeholder="Enter your location or vehicle type"
-      />
-      <button class="btn btn-primary ms-3">Search</button>
+  <navbar></navbar>
+  <div class="bg-light">
+    <div id="content" class="content-wrapper d-flex justify-content-center align-items-center">
+      <div class="p-5 d-flex flex-column align-items-center justify-content-center" id="bg-warning">
+        <div class="text-center mb-4">
+          <h1 class="text-white">Where to Go?</h1>
+          <p class="text-white">Find vehicles available in Cambodia</p>
+        </div>
+        <div class="d-flex justify-content-center">
+          <input
+            type="text"
+            v-model="vehicle"
+            class="form-control"
+            style="width: 300px"
+            placeholder="Enter your location or vehicle type"
+          />
+          <button class="btn btn-primary ms-3">Search</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-
-      </div>
-      
-    </div>
-    <div class="d-flex justify-content-center">
-      <div class="card m-3" style="width: 18rem">
-        <img
-          class="card-img-top"
-          src="https://i.pinimg.com/474x/fd/ec/31/fdec319b8efbdb17a28278c743d9ffaf.jpg"
-          style="height: 150px; object-fit: cover"
-          alt="Card image cap"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Card Title 1</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </p>
-        </div>
-      </div>
-
-      <div class="card m-3" style="width: 18rem">
-        <img
-          class="card-img-top"
-          src="https://i.pinimg.com/474x/92/2f/24/922f2494e08203b4d8014da570153774.jpg"
-          style="height: 150px; object-fit: cover"
-          alt="Card image cap"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Card Title 2</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </p>
-        </div>
-      </div>
-    </div>
+  <hotel></hotel>
 </template>
   
   <script>
+import hotel from '@/Components/Traveler/HotelTraveler.vue'
 import Navbar from '@/Components/Traveler/navbarTraveler.vue'
-
 export default {
   name: 'place-traveler',
   components: {
-    Navbar
+    Navbar,
+    hotel
   },
   data() {
     return {
