63d6588561aaec3c7e6a195bda5ef45e8b4f9fdf
LATY.OUN <LATY.OUN@PNC.LAN>
2024-07-04 10:26:02 +0700

    show list top GE-110

--- frontend vc2 pnc/src/Components/Traveler/PlaceTraveler.vue
+++ frontend vc2 pnc/src/Components/Traveler/PlaceTraveler.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="container">
     <div class="row">
       <div class="col-md-2 col-6" v-for="place in places" :key="place.id">
@@ -7,7 +8,12 @@
           <div class="card-body">
             <h5 class="card-title">{{ place.title }}</h5>
             <p class="card-text">{{ place.location }}</p>
-            <a href="#" class="btn btn-primary">Visit</a>
+            <!-- <div class="rating"> -->
+              <star-rating :rating="place.rating" />
+            <!-- </div> -->
+            <hr>
+            
+            <a href="/place-detail" class="btn btn-primary">View Detail</a>
           </div>
         </div>
       </div>
@@ -16,40 +22,50 @@
 </template>
 
 <script>
+import StarRating from '@/Components/Traveler/StarRating.vue'
 export default {
   name: 'PlacesPage',
+  components: {
+    StarRating
+  },
+  
   data() {
     return {
       places: [
         {
           id: 1,
-          image: 'https://i.pinimg.com/474x/66/b9/fa/66b9fa7a25157bd08201d25acd5ad7e5.jpg',
+          image: 'https://www.areacambodia.com/wp-content/uploads/2023/09/Experiencing-the-Magical-Equinox-Sunrise-at-Angkor-Wat-A-Must-See-in-Cambodia-2023.jpg',
           title: 'Angkor Wat',
-          location: 'Siem Reab'
+          location: 'Siem Reab',
+          rating:4.5
         },
         {
           id: 2,
-          image: 'https://i.pinimg.com/564x/33/ba/a9/33baa93e78675223f52fe831173f9e42.jpg',
+          image: 'https://bayontabitour.com/userfiles/udong-att-b.jpg',
           title: 'Phnom Oudong',
-          location: 'Kompong Spue'
+          location: 'Kompong Spue',
+          rating:5
         },
         {
-          id: 1,
-          image: 'https://i.pinimg.com/474x/66/b9/fa/66b9fa7a25157bd08201d25acd5ad7e5.jpg',
-          title: 'Angkor Wat',
-          location: 'Siem Reab'
+          id: 3,
+          image: 'https://www.renown-travel.com/images/royal-palace-phnom-penh-l.jpg',
+          title: 'Royal Palace',
+          location: 'Phnom Penh',
+          rating:4.5
         },
         {
-          id: 2,
-          image: 'https://i.pinimg.com/564x/33/ba/a9/33baa93e78675223f52fe831173f9e42.jpg',
-          title: 'Phnom Oudong',
-          location: 'Kompong Spue'
+          id: 4,
+          image: 'https://static.arocdn.com/Sites/50/songsaa/uploads/images/Gallery/nosize29/island_from_above.jpg',
+          title: 'Koh Rong',
+          location: 'Kampot',
+          rating:4.5
         },
         {
-          id: 2,
-          image: 'https://i.pinimg.com/564x/33/ba/a9/33baa93e78675223f52fe831173f9e42.jpg',
-          title: 'Phnom Oudong',
-          location: 'Kompong Spue'
+          id: 5,
+          image: 'https://qph.cf2.quoracdn.net/main-qimg-edcb9b21e281a7d12b10eeecc52e0b07-lq',
+          title: 'Preah Vihear Temple',
+          location: 'Preah Vihear',
+          rating:4.5
         },
       ]
     }
@@ -59,9 +75,17 @@ export default {
 
 <style scoped>
 .container {
-  margin-top: 2rem;
+  /* margin-top: -20px; */
   display: flex;
   justify-content: center;
+}
+.card-title{
+  color: black;
+  font: bold 25px "Times New Roman";
+}
+.card-text{
+  color: gray;
+  font-size: 15px;
 }
 
 .row {
@@ -80,10 +104,54 @@ export default {
 
 .card {
   width: 100%;
+  background-color: #f8f8f8;
+  border: none;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s, box-shadow 0.3s;
+}
+
+.card:hover {
+  
+  box-shadow: 10px 20px 30px rgba(0, 0, 0, 0.2);
 }
 
 .card-img-top {
-  height: 120px;
+  height: 200px;
   object-fit: cover;
+  
+}
+
+.card-body {
+  padding: 1rem;
+}
+
+.card-title {
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+}
+
+.card-text {
+  margin-bottom: 0.5rem;
+}
+
+.btn {
+  width: 100%;
+ 
+}
+
+.btn-primary {
+  background-color: #007bff;
+  color: #fff;
+  padding: 0.5rem 1rem;
+}
+
+.btn-primary {
+  background-color: blue;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+}
+.btn-primary:hover{
+  background:red;
 }
 </style>
