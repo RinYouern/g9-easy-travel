820f214237037fa63b9ef2df461c2bbf8455059a
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-07-08 10:56:27 +0700

    integrate api with form add new place GE-106

--- backend vc2 pnc/app/Models/Place.php
+++ backend vc2 pnc/app/Models/Place.php
@@ -8,28 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Place extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'description', 'rate', 'location', 'images'];
-    
 
-    /**
-     * Set the images attribute and encode it to JSON.
-     *
-     * @param array $value
-     * @return void
-     */
-    public function setImageAttribute($value)
-    {
-        $this->attributes['images'] = json_encode($value);
-    }
+    protected $fillable = ['name', 'description', 'location', 'images'];
 
-    /**
-     * Get the images attribute and decode it from JSON.
-     *
-     * @param string $value
-     * @return array
-     */
-    public function getImageAttribute($value)
-    {
-        return json_decode($value, true);
-    }
+    protected $casts = [
+        'images' => 'json',
+    ];
 }
