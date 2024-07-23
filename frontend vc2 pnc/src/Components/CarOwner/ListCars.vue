070b1a54dccb3be7ce0e0f2c66a427f4ac081c8e
LATY.OUN <LATY.OUN@PNC.LAN>
2024-07-23 18:39:21 +0700

    [imp] improve carowner page

--- frontend vc2 pnc/src/Components/CarOwner/ListCars.vue
+++ frontend vc2 pnc/src/Components/CarOwner/ListCars.vue
@@ -1,18 +1,19 @@
 <template>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <div class="container-fluid dashboard">
     <div class="row">
       <sidebar /> 
       <main class="col-md-9 col-lg-10 main-content p-4">
         <div class="d-flex align-items-center justify-content-between mb-3">
-          <h4 class="text-left text-black font-weight-bold">
+          <h4 class="text-left text-black font-weight-bold" style="font-size:30px">
             Vehicles List
           </h4>
-          <button class="btn btn-primary" lain @click="dialogTableVisible = true">+Add Vehicle</button>
+          <button class="btn btn-primary" lain @click="dialogTableVisible = true"> <strong style="font-size:20px">+</strong> <i class="fa fa-bus" style="font-size:20px"></i></button>
         </div>
         <vehecles/>
       </main>
       
-      <el-dialog v-model="dialogTableVisible" width="600" style="padding: 5%">
+      <el-dialog v-model="dialogTableVisible" width="650" height="30px" style="padding: 5%">
     <h2 class=" font-bold mb-6 text-center" >Add Vehicle</h2>
     <el-form :model="formData" :rules="formRules" ref="vehicleForm" label-width="120px">
       <el-row :gutter="20">
@@ -62,8 +63,8 @@
 
       <el-row :gutter="20" class="justify-content-center">
         <el-col :span="20">
-          <el-form prop="description" :error="descriptionError">
-            <el-input placeholder="Description" v-model="formData.description"  type="textarea" width="50" />
+          <el-form prop="description" :error="descriptionError" >
+            <el-input placeholder="Description" v-model="formData.description"  type="textarea" width="50" style="border:1px solid grey" />
          
           </el-form>
         </el-col>
@@ -138,19 +139,13 @@ export default {
 }
 </script>
 <style>
-  /* Container styles */
-  /* .container-fluid.dashboard {
-    padding: 20px;
-  } */
-
-  /* Heading styles */
+  
   h4 {
     font-size: 24px;
     font-weight: bold;
     
   }
 
-  /* Button styles */
   .btn-primary {
     margin-left: 10px;
   }
@@ -158,11 +153,12 @@ export default {
   .el-form {
     margin-bottom: 10px;
     padding: 5px;
+    
   }
 
   .el-input {
     width: 100%;
-    /* border: 1px solid black; */
+    border: 1px solid grey;
   }
   .el-button {
     width: 15%;
